9a012433c2d06890548be80e1970161ae6417205
mikus <dont@think.so>
2016-11-12 21:25:38 +0200

    Added gson jar

---
+++ src/lv/mikust/tehbot/ExtractUri.java
@@ -0,0 +1,33 @@
+/**
+ * Created by mikus on 2016.11.11.
+ */
+package lv.mikust.tehbot;
+
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractUri {
+
+    public String splitText(String fullText) {
+        StringTokenizer stringTokenizer = new StringTokenizer(fullText, " ");
+        while (stringTokenizer.hasMoreTokens()) {
+            String token = stringTokenizer.nextToken();
+            if (testURI(token) != null) {
+                return token;
+            }
+        }
+        return null;
+    }
+
+    // Returns domain name if matches regex
+    public String testURI(String word) {
+        Pattern pattern = Pattern.compile("^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{1,6}$");
+        Matcher matcher = pattern.matcher(word);
+        if (matcher.matches()) {
+            return "";
+        }
+        return null;
+    }
+
+}
