f2d3f2f6c33de040dde3901fa3a53e5d08d9b374
mikus <dont@think.so>
2016-11-12 21:25:38 +0200

    Added rudimentary message parser. Bot can speak now

--- src/lv/mikust/tehbot/TweetBot.java
+++ src/lv/mikust/tehbot/TweetBot.java
@@ -1,22 +1,52 @@
 /**
  * Created by mikus on 07/11/2016.
  */
-
 package lv.mikust.tehbot;
 
 import twitter4j.*;
 
-public final class TweetBot {
+public class TweetBot {
+    private static final Logger logger = Logger.getLogger(TwitterStream.class);
     public static void main(String[] args) throws TwitterException {
         TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
         twitterStream.addListener(listener);
         twitterStream.user();
+        WebRequests webRequests = new WebRequests();
     }
 
-    private static final UserStreamListener listener = new UserStreamListener() {
+    // Stream listener
+
+    private static UserStreamListener listener = new UserStreamListener() {
         @Override
         public void onStatus(Status status) {
-            System.out.println("onStatus @" + status.getUser().getScreenName() + " - " + status.getText());
+            logger.info("Message: " + status.getUser().getScreenName() + " - " + status.getText());
+            Twitter twitter = new TwitterFactory().getInstance();
+
+            // Responds with help message
+            if (status.getText().toLowerCase().contains("!help")) {
+                StatusUpdate statusUpdate = new StatusUpdate("@" + status.getUser().getScreenName() + " sup?");
+                statusUpdate.inReplyToStatusId(status.getId());
+                try {
+                    twitter.updateStatus(statusUpdate);
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+
+                // Prints info about webpage
+            } else if (status.getText().toLowerCase().contains("!d")) {
+                ExtractUri extractUri = new ExtractUri();
+                if (extractUri.splitText(status.getText()) != null) {
+                    String domainName = extractUri.splitText(status.getText());
+                    try {
+                        logger.info("Sending GET / request to " + domainName);
+                        StatusUpdate statusUpdate = new StatusUpdate("@" + status.getUser().getScreenName() + WebRequests.getGet(domainName));
+                        statusUpdate.inReplyToStatusId(status.getId());
+                        twitter.updateStatus(statusUpdate);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
 
         @Override
